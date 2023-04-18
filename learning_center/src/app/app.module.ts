c27f6c9e5f573935004240da38eea03ce15ca5e2
Amal-Abdelhamed <amalabdelhameed90amalabdelhame@gmail.com>
2023-04-18 21:03:18 +0200

    add stucture pages&components

--- learning_center/src/app/app.module.ts
+++ learning_center/src/app/app.module.ts
@@ -3,10 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IndexComponent } from './pages/index/index.component';
+import { CoursesComponent } from './pages/courses/courses.component';
+import { ContactUsComponent } from './pages/contact-us/contact-us.component';
+import { LoginComponent } from './pages/login/login.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { EditProfileComponent } from './pages/edit-profile/edit-profile.component';
+import { EditCourseComponent } from './pages/edit-course/edit-course.component';
+import { AddCourseComponent } from './pages/add-course/add-course.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CardComponent } from './components/card/card.component';
+import { FormComponent } from './components/form/form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    CoursesComponent,
+    ContactUsComponent,
+    LoginComponent,
+    DashboardComponent,
+    ProfileComponent,
+    EditProfileComponent,
+    EditCourseComponent,
+    AddCourseComponent,
+    NavbarComponent,
+    FooterComponent,
+    CardComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
